db7454b5ad2eadc3915d6ef9204c1c5f6c85469f
complexity-coding <85390274+informatics-mathematics@users.noreply.github.com>
2021-08-07 01:28:43 +0100

    Update and rename Relativity - RELATIVE.md to Relativity - (RELATIVE).cpp

--- Long Challenge/July 2021/Relativity - (RELATIVE).cpp
+++ Long Challenge/July 2021/Relativity - (RELATIVE).cpp
@@ -1,17 +1,17 @@
-```
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 	int T, g, c;
 
 	cin >> T;
 
-	for(int i = 1; i <= T; i++){
+	for(int i = 1; i <= T; i++)
+	{
 		cin >> g >> c;
 		cout << (c*c)/(2*g) <<"\n";
 	}
 
 	return 0;
 }
-```
